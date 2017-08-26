744ee36b3b2bd6ff57e6169cd3a1b66558f96386
Kado Damball <kadodamball@hotmail.com>
2017-08-26 10:55:21 +0200

    Example for making a request

--- index.js
+++ index.js
@@ -3,14 +3,28 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', function (req, res) {
-  res.json({
+app.get('/', function (request, response) {
+  response.json({
     name: "Valentin"
   })
 });
 
-app.get('/vote', function (req, res) {
-  res.json({
+app.get('/vote', function (request, response) {
+  /*
+    Inside response.json(), you just pass a normal JS object
+    It will be converted to a JSON and sent back
+
+    e.g: function getPerson(){
+      return {
+        name: "Valentin",
+        location: "Berlin",
+        legs: 2
+      }
+
+      response.json(getPerson()) will return the object back as a response
+    }
+  */
+  response.json({
     name: "Kado"
   })
 });
