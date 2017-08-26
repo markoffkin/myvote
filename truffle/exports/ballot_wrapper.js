cd87494294f1ccb838390b1d22af9619404611fa
valentin markounikau <ganganga@gmail.com>
2017-08-26 14:23:45 +0200

    Fix ballot_wrapper.js -- create local web3 provider. WIP: Cannot read property getWinnerName of undefined

--- truffle/exports/ballot_wrapper.js
+++ truffle/exports/ballot_wrapper.js
@@ -1,5 +1,5 @@
 // Import libraries we need.
-const web3 = require( 'web3');
+const Web3 = require( 'web3');
 const contract = require('truffle-contract')
 
 // Import our contract artifacts and turn them into usable abstractions.
@@ -7,6 +7,13 @@ const ballot_artifacts = require('../build/contracts/Ballot.json')
 
 class BallotWrapper {
 	constructor() {
+               
+    		// set the provider you want from Web3.providers
+    		var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));   
+    		if(!web3.isConnected())
+        		console.log("not connected");
+    		else
+        		console.log("connected");
 		this.Ballot = contract(ballot_artifacts);
 		this.Ballot.setProvider(web3.currentProvider);
 	}
